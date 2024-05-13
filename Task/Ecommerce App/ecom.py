334bac6c5299d5488b73825035164b7cf7c307b0
Akpatel48 <148442756+Akpatel48@users.noreply.github.com>
2024-05-13 11:48:47 +0530

    ecom

--- Task/Ecommerce App/ecom.py
+++ Task/Ecommerce App/ecom.py
@@ -8,7 +8,7 @@ except Exception as er:
 cr=db.cursor()
 #create table
 try:
-    table='CREATE table item(id integer PRIMARY KEY,sname varchar(20),name varchar(20),price integer,qui integer)'
+    table='CREATE table item(id integer PRIMARY KEY AUTO_INCREMENT,sname varchar(20),name varchar(20),price integer,qui integer)'
     cr.execute(table)
     print("table created")
 except Exception as er:
@@ -16,30 +16,95 @@ except Exception as er:
 
 class manger:
     def input(self):
-        id=int(input("enter id:"))
-        sname=input("enter saler name:")
-        name=input("enter prodect name:")
-        price=input("enter price:")
-        quit=input("enter quit:")
         try:
-            cr.execute('insert into item values(%s,%s,%s,%s,%s)', (id,sname,name,price,quit)) 
-            db.commit()
+            sname = input("Enter seller name: ")
+            name = input("Enter product name: ")
+            price = int(input("Enter price: "))
+            quantity = int(input("Enter quantity: "))
+
+            try:
+                cr.execute('INSERT INTO item(sname,name,price,qui) VALUES (%s, %s, %s, %s)', (sname, name, price, quantity))
+                db.commit()
+                print("Product inserted successfully!")
+            except Exception as er:
+                print("Error inserting product:", e)
+        except ValueError:
+            print("Invalid input. Please enter valid integer values for ID, price, and quantity.")
         except Exception as er:
-            price(er)
+            print("Error:", er)
+
     def update(self):
-        up=int(input("enter prodect id:"))
         try:
-            upd=cr.execute('select id from item where id=%s',(up))
-            if up==upd:
-                uname=input("enter prodect name:")
-                uprice=input('enter price:')
-                uquit=input("enter quit")
+            up = int(input("Enter product id: "))
+            cr.execute('SELECT id FROM item WHERE id=%s', (up,))
+            upd = cr.fetchone()
+        
+            if upd:
+                uname = input("Enter product name: ")
+                uprice = input('Enter price: ')
+                uquit = input("Enter quantity: ")
+            
                 try:
-                    cr.execute("update item ")
+                    cr.execute('UPDATE item SET name=%s, price=%s, qui=%s WHERE id=%s', (uname, uprice, uquit, upd[0]))
+                    db.commit()
+                    print("Product updated successfully!")
+                except Exception as er:
+                    print("Error updating product:", er)
             else:
-                print("not aveleble")
+                print("Product ID not found!")
         except Exception as er:
             print(er)
-        
+
+    def view(self):
+        try:
+            cr.execute('select * from item')
+            vie=cr.fetchall()
+            for i in vie:
+                print(i)
+        except Exception as er:
+            print(er)
+
+class customer():
+    def purchase(self):
+        try:
+            name = input("Enter product name: ")
+            cr.execute('SELECT id,qui FROM item WHERE name=%s', (name))
+            buy = cr.fetchone()
+            if buy:
+                bquit =int(input("Enter quantity: "))
+                if buy[1]>bquit:
+                    temp=buy[1]
+                    bquit=temp-bquit
+                    try:
+                        cr.execute('UPDATE item SET qui=%s WHERE id=%s', (bquit,buy[0]))
+                        db.commit()
+                        print("Product buy successfully!")
+                    except Exception as er:
+                        print("Error buy product:", er)
+                else:
+                    print("Product quit not inaf !")
+            else:
+                print("Product NAME not found!")
+        except Exception as er:
+            print(er)
 ma=manger()
-ma.update()
+ca=customer()
+def display():
+    print("\t\t\tWelcome to Market")
+    print('1.Product Manger\n2.Customer')
+    try:
+        role=int(input("Enter your role:"))
+    except Exception as er:
+        print(er)
+    if role==1:
+        print("1.INSER\n2.UPDATE\n3.VIEW")
+        select=int(input("Enter you prform opresen:"))
+        if select==1:
+            ma.input()
+        elif select==2:
+            ma.update()
+        elif select==3:
+            ma.view()
+    elif role==2:
+        ca.purchase()
+display()
