3de94e33d26d85f69de92c323577032e8146dbdf
Akpatel48 <ayushkamani956@gmail.com>
2024-07-10 17:38:10 +0530

    module 5

--- assignment/module 5/p7/p7/asgi.py
+++ assignment/module 5/p7/p7/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for p6 project.
+ASGI config for p7 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'p6.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'p7.settings')
 
 application = get_asgi_application()
--- assignment/module 5/p7/p7/urls.py
+++ assignment/module 5/p7/p7/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for p6 project.
+URL configuration for p7 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
