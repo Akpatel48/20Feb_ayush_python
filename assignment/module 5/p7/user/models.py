2d9ca5e9a6cb576f191d5614ae0a8679ef5bbad8
Akpatel48 <ayushkamani956@gmail.com>
2024-07-11 18:23:35 +0530

    module 5

--- assignment/module 5/p7/user/models.py
+++ assignment/module 5/p7/user/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class prodect(models.Model):
     p_name=models.CharField(max_length=50)
+    def __str__(self):
+        return self.p_name
 
 class sub_prodct(models.Model):
     p_name=models.ForeignKey(prodect,on_delete=models.CASCADE)
@@ -10,3 +12,6 @@ class sub_prodct(models.Model):
     p_img=models.ImageField()
     p_model=models.CharField(max_length=50)
     p_ram=models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.p_name.p_name
